6c1b801c708cd62fc6336505729b16336adf74f1
Linh-Bau <58115090+Linh-Bau@users.noreply.github.com>
2023-04-15 16:41:05 +0700

    a

--- Third/example.py
+++ Third/example.py
@@ -4,8 +4,7 @@ from mnist import MNIST # require `pip install python-mnist`
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from display_network import *
-
-from display_network import *
+from convert import *
 
 #load dữ liệu
 mndata = MNIST('./MNIST/') # path to your MNIST folder 
@@ -19,22 +18,22 @@ K=10
 kmeans=KMeans(n_clusters=K,random_state=0).fit(X)
 pred_labels=kmeans.predict(X)
 
+M=None
 
 #lấy ramdom 20 bức ảnh ứng với mỗi label
-M=None
 for k in range(0,10):
     k_cluster=X[pred_labels==k,:]
-    ramdom_index=np.random.choice(k_cluster.shape[0],1,replace=False)
+    ramdom_index=np.random.choice(k_cluster.shape[0],20,replace=False)
     k_cluster_data=k_cluster[ramdom_index,:].ravel()
     if M is None:
-        M= k_cluster_data
+        M=k_cluster_data
     else:
-        M= np.vstack((M, k_cluster[ramdom_index,:].ravel().T))
+        M= np.vstack((M, k_cluster_data))
 
-S=np.vstack((kmeans.cluster_centers_.T,M.T))
+S=np.hstack((kmeans.cluster_centers_,M))
 
+A=convert_matrix(S,28,28)
 
-A=display_network(S,10,2)
 
 f1 = plt.imshow(A, interpolation='nearest', cmap = "gray")
 
